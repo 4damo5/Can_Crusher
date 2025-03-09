836ed1442db23cdab38b8bebe2767862b06b1320
4damo5 <adamotsuka3@gmail.com>
2025-03-09 22:34:03 +0000

    formatting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-#include "mbed.h"
 #include "crusher_system.h"
+#include "mbed.h"
 
 // main() runs in its own thread in the OS
-int main()
-{
-    crusherSystemInit();
-    
-    while (true) {
-        crusherSystemUpdate();
-    }
-}
+int main() {
+  crusherSystemInit();
 
+  while (true) {
+    crusherSystemUpdate();
+  }
+}
